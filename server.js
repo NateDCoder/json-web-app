dbccb5abda314d257ff92b92fc556068d0c4c2bc
NateDCoder <rubyandtwixie@gmail.com>
2024-12-01 15:37:27 -0500

    FINALLY got the data for event working

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const cors = require('cors');
+const { pathToFileURL } = require('url');
 
 const app = express();
 app.use(cors());
@@ -96,7 +97,7 @@ app.post('/api/update', (req, res) => {
 });
 async function update(token) {
     await updateAllEventData(token);
-    updateTeamData()
+    await updateTeamData(token)
 }
 // POST endpoint to reset data
 app.post('/api/reset', (req, res) => {
@@ -112,7 +113,17 @@ app.post('/api/reset', (req, res) => {
         res.status(500).json({ error: 'Error resetting data' });
     }
 });
-
+const eventCodesGlobal = JSON.parse(fs.readFileSync("./data/Event Codes.json", "utf8"));
+for (let eventCode of eventCodesGlobal) {
+    app.get('/api/'+eventCode, (req, res) => {
+        try {
+            const data = readJson("./data/Event Details/" + eventCode + ".json");
+            res.json(data);
+        } catch (error) {
+            res.status(500).json({ error: 'Error reading eventNamesPath' });
+        }
+    });
+}
 // Start server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
@@ -174,6 +185,7 @@ async function updateEloRatingsOverTime(data) {
     writeJson(data2Path, eloRatingsOverTime)
 }
 async function updateAllEventData(token) {
+    return
     const eventCodes = JSON.parse(fs.readFileSync("./data/Event Codes.json", "utf8"));
     // Define the API URL you want to call
     for (let event_code of eventCodes) {
@@ -251,7 +263,7 @@ async function updateAllEventData(token) {
 
     }
 }
-function updateTeamData() {
+async function updateTeamData(token) {
     const gameMean = 36.52564102564103;
     const autoMean = 2.7083333333333335;
     const teleOpMean = 21.166666666666668;
@@ -259,7 +271,7 @@ function updateTeamData() {
     const std = 25.282160621427234;
     const allTeams = JSON.parse(fs.readFileSync("./data/Team List.json", "utf8"));
     const eventCodes = JSON.parse(fs.readFileSync("./data/Event Codes.json", "utf8"));
-    const numbterToName = JSON.parse(fs.readFileSync("./data/Number To Team.json", "utf8"));
+    const numberToName = JSON.parse(fs.readFileSync("./data/Number To Team.json", "utf8"));
 
     var teamData = {};
     var eloRatingsOverTime = {};
@@ -275,12 +287,41 @@ function updateTeamData() {
         };
     });
 
-    console.log(eventCodes)
     for (let eventCode of eventCodes) {
         let matches = JSON.parse(fs.readFileSync(`./data/Event Matches/${eventCode}.json`, "utf8"));
         let matchesTeams = JSON.parse(fs.readFileSync(`./data/Event Matches/${eventCode}_Team.json`, "utf8"));
         var eventData = {}
-        console.log()
+
+        var teams = await getTeamsAtEvent(eventCode, token);
+        var schedule = await getScheduleAtEvent(eventCode, token);
+        var ranks = await getRanks(eventCode, token);
+
+        var teamNumbers = teams.teams.map(team => team.teamNumber);
+        if (teamNumbers.length == 0) {
+            eventData["hasTeamList"] = false;
+        } else {
+            eventData["hasTeamList"] = true;
+        }
+
+        if (schedule.schedule.length == 0) {
+            eventData["hasScheduleList"] = false;
+        } else {
+            eventData["hasScheduleist"] = true;
+        }
+
+        if (ranks.rankings.length == 0) {
+            eventData["hasRankings"] = false;
+        } else {
+            eventData["hasRankings"] = true;
+        }
+
+        if (eventData["hasTeamList"]) {
+            console.log(eventCode)
+            eventData["preEloTeamList"] = getTeamData({...teamData}, teamNumbers, numberToName, [...ranks.rankings])
+        }
+
+        eventData["teams"] = teamNumbers
+        eventData["schedule"] = schedule.schedule
         for (let i = 0; i < matches.matchScores.length; i++) {
             let matchData = matches.matchScores[i].alliances;
             let blueScore = 0;
@@ -344,42 +385,48 @@ function updateTeamData() {
             let bluePointDifference = blueScore - (teamData[blue1]["EPA"] + teamData[blue2]["EPA"])
             let redPointDifference = redScore - (teamData[red1]["EPA"] + teamData[red2]["EPA"])
 
-            teamData[blue1]["EPA"] += getK(teamData[blue1]["Played Matches"]) * bluePointDifference;
-            teamData[blue2]["EPA"] += getK(teamData[blue2]["Played Matches"]) * bluePointDifference;
+            teamData[blue1]["EPA"] += getK(teamData[blue1]["Played Matches"], matches.matchScores[i].matchLevel) * bluePointDifference;
+            teamData[blue2]["EPA"] += getK(teamData[blue2]["Played Matches"], matches.matchScores[i].matchLevel) * bluePointDifference;
 
-            teamData[red1]["EPA"] += getK(teamData[red1]["Played Matches"]) * redPointDifference;
-            teamData[red2]["EPA"] += getK(teamData[red2]["Played Matches"]) * redPointDifference;
+            teamData[red1]["EPA"] += getK(teamData[red1]["Played Matches"], matches.matchScores[i].matchLevel) * redPointDifference;
+            teamData[red2]["EPA"] += getK(teamData[red2]["Played Matches"], matches.matchScores[i].matchLevel) * redPointDifference;
 
 
             let blueAutoDifference = blueAuto - (teamData[blue1]["Auto EPA"] + teamData[blue2]["Auto EPA"])
             let redAutoDifference = redAuto - (teamData[red1]["Auto EPA"] + teamData[red2]["Auto EPA"])
 
-            teamData[blue1]["Auto EPA"] += getK(teamData[blue1]["Played Matches"]) * blueAutoDifference;
-            teamData[blue2]["Auto EPA"] += getK(teamData[blue2]["Played Matches"]) * blueAutoDifference;
+            teamData[blue1]["Auto EPA"] += getK(teamData[blue1]["Played Matches"], matches.matchScores[i].matchLevel) * blueAutoDifference;
+            teamData[blue2]["Auto EPA"] += getK(teamData[blue2]["Played Matches"], matches.matchScores[i].matchLevel) * blueAutoDifference;
 
-            teamData[red1]["Auto EPA"] += getK(teamData[red1]["Played Matches"]) * redAutoDifference;
-            teamData[red2]["Auto EPA"] += getK(teamData[red2]["Played Matches"]) * redAutoDifference;
+            teamData[red1]["Auto EPA"] += getK(teamData[red1]["Played Matches"], matches.matchScores[i].matchLevel) * redAutoDifference;
+            teamData[red2]["Auto EPA"] += getK(teamData[red2]["Played Matches"], matches.matchScores[i].matchLevel) * redAutoDifference;
 
 
             let blueTeleOpDifference = blueTeleOp - (teamData[blue1]["TeleOp EPA"] + teamData[blue2]["TeleOp EPA"])
             let redTeleOpDifference = redTeleOp - (teamData[red1]["TeleOp EPA"] + teamData[red2]["TeleOp EPA"])
 
-            teamData[blue1]["TeleOp EPA"] += getK(teamData[blue1]["Played Matches"]) * blueTeleOpDifference;
-            teamData[blue2]["TeleOp EPA"] += getK(teamData[blue2]["Played Matches"]) * blueTeleOpDifference;
+            teamData[blue1]["TeleOp EPA"] += getK(teamData[blue1]["Played Matches"], matches.matchScores[i].matchLevel) * blueTeleOpDifference;
+            teamData[blue2]["TeleOp EPA"] += getK(teamData[blue2]["Played Matches"], matches.matchScores[i].matchLevel) * blueTeleOpDifference;
 
-            teamData[red1]["TeleOp EPA"] += getK(teamData[red1]["Played Matches"]) * redTeleOpDifference;
-            teamData[red2]["TeleOp EPA"] += getK(teamData[red2]["Played Matches"]) * redTeleOpDifference;
+            teamData[red1]["TeleOp EPA"] += getK(teamData[red1]["Played Matches"], matches.matchScores[i].matchLevel) * redTeleOpDifference;
+            teamData[red2]["TeleOp EPA"] += getK(teamData[red2]["Played Matches"], matches.matchScores[i].matchLevel) * redTeleOpDifference;
 
 
             let blueEndgameDifference = blueEndgame - (teamData[blue1]["Endgame EPA"] + teamData[blue2]["Endgame EPA"])
             let redEndgameDifference = redEndgame - (teamData[red1]["Endgame EPA"] + teamData[red2]["Endgame EPA"])
 
-            teamData[blue1]["Endgame EPA"] += getK(teamData[blue1]["Played Matches"]) * blueEndgameDifference;
-            teamData[blue2]["Endgame EPA"] += getK(teamData[blue2]["Played Matches"]) * blueEndgameDifference;
+            teamData[blue1]["Endgame EPA"] += getK(teamData[blue1]["Played Matches"], matches.matchScores[i].matchLevel) * blueEndgameDifference;
+            teamData[blue2]["Endgame EPA"] += getK(teamData[blue2]["Played Matches"], matches.matchScores[i].matchLevel) * blueEndgameDifference;
 
-            teamData[red1]["Endgame EPA"] += getK(teamData[red1]["Played Matches"]) * redEndgameDifference;
-            teamData[red2]["Endgame EPA"] += getK(teamData[red2]["Played Matches"]) * redEndgameDifference;
+            teamData[red1]["Endgame EPA"] += getK(teamData[red1]["Played Matches"], matches.matchScores[i].matchLevel) * redEndgameDifference;
+            teamData[red2]["Endgame EPA"] += getK(teamData[red2]["Played Matches"], matches.matchScores[i].matchLevel) * redEndgameDifference;
         }
+        if (eventData["hasTeamList"]) {
+            eventData["afterEloTeamList"] = getTeamData({...teamData}, teamNumbers, numberToName, [...ranks.rankings])
+        }
+        eventData["rankings"] = ranks.rankings;
+
+        fs.writeFileSync("./data/Event Details/" + eventCode + ".json", JSON.stringify(eventData, null, 2))
     }
     let allEPA = [];
     for (let teamNumber of allTeams) {
@@ -392,7 +439,7 @@ function updateTeamData() {
     let allTeamData = [];
     for (let teamNumber of allTeams) {
         let untilessEPA = 1500 + 250 * (teamData[teamNumber]["EPA"] - averageEPA) / standardDevation
-        allTeamData.push({ "Number": teamNumber, "Name": numbterToName[teamNumber], "Unitless EPA": untilessEPA, "EPA": teamData[teamNumber]["EPA"], "Auto EPA": teamData[teamNumber]["Auto EPA"], "TeleOp EPA": teamData[teamNumber]["TeleOp EPA"], "Endgame EPA": teamData[teamNumber]["Endgame EPA"] })
+        allTeamData.push({ "Number": teamNumber, "Name": numberToName[teamNumber], "Unitless EPA": untilessEPA, "EPA": teamData[teamNumber]["EPA"], "Auto EPA": teamData[teamNumber]["Auto EPA"], "TeleOp EPA": teamData[teamNumber]["TeleOp EPA"], "Endgame EPA": teamData[teamNumber]["Endgame EPA"] })
     }
 
     allTeamData.sort((a, b) => b["Auto EPA"] - a["Auto EPA"]);
@@ -422,8 +469,11 @@ function updateTeamData() {
     allTeamData.forEach((event, index) => {
         event["EPA Rank"] = index + 1
     });
+
+    fs.writeFileSync(teamListInfoPath, JSON.stringify(allTeamData, null, 2))
 }
-function getK(gamesPlayed) {
+function getK(gamesPlayed, matchLevel) {
+    if (matchLevel == "PLAYOFF") return 0;
     if (gamesPlayed <= 6) {
         return 0.5;
     } else if (gamesPlayed <= 12) {
@@ -431,4 +481,133 @@ function getK(gamesPlayed) {
     } else {
         return 0.3
     }
+}
+
+async function getTeamsAtEvent(event_code, token) {
+    const apiUrl = "https://ftc-api.firstinspires.org/v2.0/2024/teams?eventCode=" + event_code; // Replace with the actual endpoint
+    console.log(apiUrl)
+    var teams
+    await fetch(apiUrl, {
+        method: "GET",
+        headers: {
+            "Authorization": `Basic ${token}`,
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => {
+            console.log("Response Status:", response.status);  // Log the status code
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.text(); // Get response as text
+        })
+        .then(data => {
+            teams = JSON.parse(data)
+        })
+        .catch(error => {
+            console.error("Error fetching the API:", error);
+        });
+    return teams;
+}
+async function getScheduleAtEvent(event_code, token) {
+    const apiUrl = "https://ftc-api.firstinspires.org/v2.0/2024/schedule/" + event_code + "/qual/hybrid"; // Replace with the actual endpoint
+    var schedule;
+    await fetch(apiUrl, {
+        method: "GET",
+        headers: {
+            "Authorization": `Basic ${token}`,
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => {
+            console.log("Response Status:", response.status);  // Log the status code
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.text(); // Get response as text
+        })
+        .then(data => {
+            schedule = JSON.parse(data)
+        })
+        .catch(error => {
+            console.error("Error fetching the API:", error);
+        });
+    return schedule;
+}
+async function getRanks(event_code, token) {
+    const apiUrl = "https://ftc-api.firstinspires.org/v2.0/2024/rankings/" + event_code; // Replace with the actual endpoint
+    var ranks;
+    await fetch(apiUrl, {
+        method: "GET",
+        headers: {
+            "Authorization": `Basic ${token}`,
+            "Content-Type": "application/json"
+        }
+    })
+        .then(response => {
+            console.log("Response Status:", response.status);  // Log the status code
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.text(); // Get response as text
+        })
+        .then(data => {
+            ranks = JSON.parse(data)
+        })
+        .catch(error => {
+            console.error("Error fetching the API:", error);
+        });
+    return ranks;
+}
+function getTeamData(teamData, teamNumbers, numberToName, ranks) {
+    let teamDataArray = [];
+    var numberToRank = {};
+    if (ranks.length > 0) {
+        ranks.forEach(item => {
+            numberToRank[item.teamNumber] = item.rank;
+        });
+    }
+    for (let teamNumber of teamNumbers) {
+        if (!teamData[teamNumber]) {
+            console.log(teamNumber)
+        }
+        var teamData1 = {...teamData[teamNumber]}
+        teamData1["Number"] = teamNumber;
+        teamData1["Name"] = numberToName[teamNumber]
+        if (ranks.length > 0) {
+            teamData1["Rank"] = numberToRank[teamNumber]
+        }
+        teamDataArray.push(teamData1)
+    }
+    teamDataArray.sort((a, b) => b["Auto EPA"] - a["Auto EPA"]);
+
+    // Add ranked rows
+    teamDataArray.forEach((event, index) => {
+        event["Auto EPA Rank"] = index + 1
+    });
+
+    teamDataArray.sort((a, b) => b["TeleOp EPA"] - a["TeleOp EPA"]);
+
+    // Add ranked rows
+    teamDataArray.forEach((event, index) => {
+        event["TeleOp EPA Rank"] = index + 1
+    });
+
+    teamDataArray.sort((a, b) => b["Endgame EPA"] - a["Endgame EPA"]);
+
+    // Add ranked rows
+    teamDataArray.forEach((event, index) => {
+        event["Endgame EPA Rank"] = index + 1
+    });
+
+    teamDataArray.sort((a, b) => b["EPA"] - a["EPA"]);
+    // Add ranked rows
+    teamDataArray.forEach((event, index) => {
+        event["EPA Rank"] = index + 1
+    });
+
+    if (ranks.length > 0) {
+        teamDataArray.sort((a, b) => a["Rank"] - b["Rank"]);
+    }
+    return teamDataArray
 }
