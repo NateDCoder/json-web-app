3e9e58b0fbfc87b012b04e9dca6212b5a29dd595
NateDCoder <rubyandtwixie@gmail.com>
2025-04-11 20:38:17 -0400

    going to push in case everythign breaks

--- redoing.js
+++ redoing.js
@@ -16,8 +16,8 @@ const readJson = (path) => JSON.parse(fs.readFileSync(path, "utf-8"));
 
 // await generateYearData(2019)
 // await calculateAverageAndSTD(2019, ["19MIQT10", "19MIQT11", "19MIQT12", "19MIQT8", "19MIQT9"]);
-await createTeamData(2019);
-// await createEventData(2019);
+// await createTeamData(2019);
+await createEventData(2019);
 
 async function createEventData(year) {
     const data = readJson(`${year}/yearData.json`);
@@ -77,14 +77,80 @@ function updateEPA(year, eventCode, matches, schedule) {
         const redData = pointBreakdown.redData;
         const blueData = pointBreakdown.blueData;
 
-        
-        console.log(pointBreakdown);
+        var redTeamsData = getTeamData(year, redTeams);
+        var blueTeamsData = getTeamData(year, blueTeams);
 
-        if (!fs.existsSync(`${year}/events/${eventCode}/matches/${matchName}.json`)) {
+        const redEPA = redTeamsData.reduce((sum, obj) => sum + (obj.totalEPA || 0), 0);
+        const blueEPA = blueTeamsData.reduce((sum, obj) => sum + (obj.totalEPA || 0), 0);
+
+        const redAutonEPA = redTeamsData.reduce((sum, obj) => sum + (obj.autonEPA || 0), 0);
+        const blueAutonEPA = blueTeamsData.reduce((sum, obj) => sum + (obj.autonEPA || 0), 0);
+
+        const redTeleOpEPA = redEPA - redAutonEPA;
+        const blueTeleOpEPA = blueEPA - blueAutonEPA;
+
+        const matchData = {};
+
+        if (Math.round(redEPA) > Math.round(blueEPA)) {
+            matchData["predictedWinner"] = "Red";
+        } else if (Math.round(redEPA) < Math.round(blueEPA)) {
+            matchData["predictedWinner"] = "Blue";
+        } else {
+            matchData["predictedWinner"] = "Tie";
         }
+        matchData["redTeams"] = redTeams;
+        matchData["blueTeams"] = blueTeams;
+
+        if (redData.redAuto > blueData.blueScore) {
+            matchData["actualWinner"] = "Red";
+        } else if (redData.redAuto < blueData.blueScore) {
+            matchData["actualWinner"] = "Blue";
+        } else {
+            matchData["actualWinner"] = "Tie";
+        }
+        matchData["pointBreakdown"] = pointBreakdown;
+        matchData["predictedScores"] = {redEPA, redAutonEPA, redTeleOpEPA, blueEPA, blueAutonEPA, blueTeleOpEPA}
+        if (!fs.existsSync(`${year}/events/${eventCode}/matches/${matchName}.json`)) {
+            writeJson(`${year}/events/${eventCode}/matches/${matchName}.json`, matchData);
+            i = 0;
+        }
+
+        let redErrorEPA = redData.redScore - redEPA;
+        let blueErrorEPA = blueData.blueScore - blueEPA;
+
+        let redAutonErrorEPA = redData.redAuto - redAutonEPA;
+        let blueAutonErrorEPA = blueData.blueAuto - blueAutonEPA;     
+        
+        redTeamsData.forEach((obj) => obj.totalEPA += redErrorEPA / (redTeamsData.length * 3))
+        blueTeamsData.forEach((obj) => obj.totalEPA += blueErrorEPA / (blueTeamsData.length * 3))
+
+        redTeamsData.forEach((obj) => obj.autonEPA += redAutonErrorEPA / (redTeamsData.length * 3))
+        blueTeamsData.forEach((obj) => obj.autonEPA += blueAutonErrorEPA / (blueTeamsData.length * 3))
+
+        redTeamsData.forEach((obj) => obj.teleOpEPA = obj.totalEPA - obj.autonEPA)
+        blueTeamsData.forEach((obj) => obj.teleOpEPA = obj.totalEPA - obj.autonEPA)
+
+        setTeamData(year, redTeamsData)
+        setTeamData(year, blueTeamsData)
+
+
+        // FIXME add wins ties losses using surrogates
+
     }
 }
-
+function setTeamData(year, teamsData) {
+    for (let team of teamsData) {
+        writeJson(`${year}/teams/${team.teamNumber}.json`);
+    }
+}
+function getTeamData(year, teamNumbers) {
+    let teamsData = [];
+    for (let teamNumber of teamNumbers) {
+        let teamData = readJson(`${year}/teams/${teamNumber}.json`);
+        teamsData.push(teamData);
+    }
+    return teamsData;
+}
 function getPointBreakdown(alliances) {
     var redScore, redPenaltyFreeScore, redAuto, redTeleOp;
 
@@ -95,13 +161,13 @@ function getPointBreakdown(alliances) {
         if (allianceColor == "Red") {
             redScore = alliance.totalPoints;
             redPenaltyFreeScore = redScore - alliance.penaltyPoints;
-            redAuto = autonomousPoints.autonomousPoints;
+            redAuto = alliance.autonomousPoints;
             redTeleOp = redPenaltyFreeScore - redAuto;
         } else if (allianceColor == "Blue") {
             blueScore = alliance.totalPoints;
-            bluePenaltyFreeScore = redScore - alliance.penaltyPoints;
-            blueAuto = autonomousPoints.autonomousPoints;
-            blueTeleOp = redPenaltyFreeScore - redAuto;
+            bluePenaltyFreeScore = blueScore - alliance.penaltyPoints;
+            blueAuto = alliance.autonomousPoints;
+            blueTeleOp = bluePenaltyFreeScore - blueAuto;
         } else {
             throw new Error("Team is neither blue or red there is an error");
         }
@@ -180,17 +246,20 @@ async function createTeamData(year) {
         }
 
         if (week1AverageAuto == null) {
-            teamStartingData["autoEPA"] = 0;
+            teamStartingData["autonEPA"] = 0;
         } else {
-            teamStartingData["autoEPA"] = eloToEPA(
+            teamStartingData["autonEPA"] = eloToEPA(
                 teamStartingData["elo"],
                 week1AverageAuto,
                 week1STDAuto
             );
         }
 
+        teamStartingData["teleOpEPA"] = teamStartingData["totalEPA"] - teamStartingData["autonEPA"];
+
         teamStartingData["totalEPAOverTime"] = [teamStartingData["totalEPA"]];
-        teamStartingData["autonEPAOverTime"] = [teamStartingData["autoEPA"]];
+        teamStartingData["teleOpEPAOverTime"] = [teamStartingData["teleOpEPA"]];
+        teamStartingData["autonEPAOverTime"] = [teamStartingData["autonEPA"]];
         writeJson(`${year}/teams/${teamNumber}.json`, teamStartingData);
     }
 }
